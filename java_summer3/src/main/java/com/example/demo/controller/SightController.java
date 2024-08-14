6a8891a79405a4d6de3810dee75d6b0a2bea9268
ZIhan <73235000+zihan0221@users.noreply.github.com>
2024-08-14 18:23:00 +0800

    add service ，還需要特判沒有文章、沒有圖片

---
+++ java_summer3/src/main/java/com/example/demo/controller/SightController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Sight;
+import com.example.demo.param.SightRequestParameter;
+import com.example.service.SightService;
+
+
+@RestController
+public class SightController {
+    
+    private final SightService sightService= new SightService();
+
+    @GetMapping("/SightAPI")
+    public ResponseEntity<List<Sight>> getSights(
+            @ModelAttribute SightRequestParameter param
+    ) {
+        String keyword = param.getZone();
+        keyword=keyword+"區";
+        ArrayList<Sight>values=sightService.getSights(keyword);
+        if (values != null) {
+            return ResponseEntity.ok(values);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
